cc0508d4d3470eeb2ede0985591ff0ec409f82f2
Abdelrahman Amr <boody2205@gmail.com>
2025-03-10 17:22:53 +0200

    Finished implementing the merge sort algorithm

--- src/main/java/com/mycompany/sorting_algorithms/MergeSort.java
+++ src/main/java/com/mycompany/sorting_algorithms/MergeSort.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.sorting_algorithms;
 
+import java.util.Arrays;
+
 /**
  *
  * @author boody
@@ -12,7 +14,46 @@ public class MergeSort implements SortingStrategy {
 
     @Override
     public int[] sort(int[] arr) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        mergeSort(arr, 0, arr.length -1);
+        return arr;
     }
     
+    private void mergeSort(int[] arr, int left, int right){
+        if(left < right){
+            int mid= (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    
+    private void merge(int[] arr, int left,int mid, int right){
+        int[] tempArr= new int[arr.length];
+        int i= left, j= mid + 1, k= left;
+        for(; i <= mid && j <= right; k++){
+            if(arr[i] < arr[j]){
+                tempArr[k]= arr[i];
+                i++;
+            }
+            else{
+                tempArr[k]= arr[j];
+                j++;
+            }
+        }
+        while(i <= mid){
+            tempArr[k++]= arr[i++];
+        }
+        while(j <= right){
+            tempArr[k++]= arr[j++];
+        }
+        for(k= left; k <= right; k++){
+            arr[k]= tempArr[k];
+        }
+    }
+    
+    public static void main(String[] args) {
+        MergeSort mySort= new MergeSort();
+        int[] arr= new int[]{3,5,8,7,9,2,1,6,15,11,12};
+        System.out.println(Arrays.toString(mySort.sort(arr)));
+    }
 }
